03fbd56a93c855e0d906e747a4eb8f16237d57c9
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-11-03 00:53:11 -0400

    added buttons to modal.

--- client/src/components/GoalsVentH1/GoalsVent.js
+++ client/src/components/GoalsVentH1/GoalsVent.js
@@ -45,20 +45,24 @@ class GoalsVent extends React.Component{
  render(){
 console.log(this.state);
         return(
+         
 <div className="GoalsVentDiv">
 <h3 className="motivation5 text-center"> TODOs <hr/></h3>
 <div className={"mapBtnDiv"}>
 
 {this.state.vent.map(item => {return (
+
   <div className="btnDiv" >
-  <button className={"mapBtn btn btn-md"} onClick={() => this.ventModal()}> {item.title}</button>
+  <button className={"mapBtn btn btn-md"} onClick={() => this.ventModal()} data-title={item.title} data-toggle="modal" data-target={`#${item._id}`}> {item.title}</button>
   </div>
+  
+ 
 )})}
 </div>
 <form id="goalVent">
 <h1 className="display-6 text-center">Enter TODOs</h1>
 <div className="form-group">
-<label for="formGroupExampleInput2">Title</label>
+<label for="formGroupExampleInput2">{}</label>
 <input type="text" className="form-control" id="ventTitle" placeholder=""/>
 </div>
 <div className="form-group">
@@ -68,6 +72,27 @@ console.log(this.state);
 
 <button type="submit" onClick={(e) => {e.preventDefault(); this.handleSubmit()}} className="btn btn-primary btn-sm submit">Submit</button>
 </form> 
+{this.state.vent.map(item => {return (
+<div className="modal fade modalVent" id={item._id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div className="modal-dialog" role="document">
+  <div className="modal-content">
+    <div className="modal-header">
+      <h3 className="modal-title" id="exampleModalLabel">{item.title}</h3>
+      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div className="modal-body">
+      {item.body}
+    </div>
+    <div className="modal-footer">
+      <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      <button type="button" className="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</div>
+</div>
+)})}
 </div>
         )
 
